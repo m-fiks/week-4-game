e2a27d571923a982e2d7f41bea38f9f277614d66
m-fiks <fiksd005@umn.edu>
2018-03-10 01:51:15 -0600

    expand gem push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,10 @@ $(document).ready(function(){
     $('#empty-words').hide();
 
 //variables
-let winCounter=0;
-let loseCounter=0;
+let winCounter="";
+let loseCounter="";
+let updatedLoseCounter=0;
+let updatedWinCounter=0;
 //if playerScore===randomNumber, win, wincounter++, reset, etc.
 let playerScore=0;
 let playerGameScore=0;
@@ -16,7 +18,6 @@ let greenButton="";
 let randomNumber="";
 const numberArray= [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
 const buttonValue= [1,2,3,4,5,6,7,8,9,10,11,12];
-
 $("#win-counter").append(winCounter);
 $("#lose-counter").append(loseCounter);
 
@@ -24,70 +25,91 @@ $("#lose-counter").append(loseCounter);
 const winner = function () {
     alert('You won!!');
     winCounter++;
-    $("#win-counter").text(winCounter);
+    updatedWinCounter += parseInt(winCounter);
+    $("#win-counter").text(parseInt(updatedWinCounter));
 };
-
 //lose function
 const loser = function () {
     alert('You lost :(');
     loseCounter++;
-    $("#lose-counter").text(loseCounter);
+    updatedLoseCounter += parseInt(loseCounter);
+    $("#lose-counter").text(parseInt(updatedLoseCounter));
 };
 
 // assign value to image buttons
-const gemPush = function () {
+const gemBluePush = function () {
     $('#blue-gem').click(function() {
-        playerGameScore += playerScore + parseInt(blueButton);
-    $('#score-counter').text(playerGameScore);
-        if (playerGameScore === parseInt(randomNumber)){
-            winner();
-        } else if (playerGameScore > parseInt(randomNumber)){
-            loser();
-        }
-    });
-    $('#crystal-gem').click(function() {
-        playerGameScore += playerScore + parseInt(crystalButton);
-    $('#score-counter').text(playerGameScore);
-        if (playerGameScore === parseInt(randomNumber)){
-            winner();
-        } else if (playerGameScore > parseInt(randomNumber)){
-            loser();
-        }
-    });
-    $('#purple-gem').click(function() {
-        playerGameScore += playerScore + parseInt(purpleButton);
-    $('#score-counter').text(playerGameScore);
-        if (playerGameScore === parseInt(randomNumber)){
-            winner();
-        } else if (playerGameScore > parseInt(randomNumber)){
-            loser();
-        }
-    });
-    $('#green-gem').click(function() {
-        playerGameScore += playerScore + parseInt(greenButton);
-    $('#score-counter').text(playerGameScore);
-        if (playerGameScore === parseInt(randomNumber)){
-            winner();
-        } else if (playerGameScore > parseInt(randomNumber)){
-            loser();
-        }
+        playerGameScore += parseInt(playerScore) + parseInt(blueButton);
+        $('#score-counter').text(parseInt(playerGameScore));
+            if (playerGameScore === parseInt(randomNumber)){
+                winner();
+            } else if (playerGameScore > parseInt(randomNumber)){
+                loser();
+            }
     });
 };
 
+const gemCrystalPush = function () {
+    $('#crystal-gem').click(function() {
+        playerGameScore += parseInt(playerScore) + parseInt(crystalButton);
+        $('#score-counter').text(parseInt(playerGameScore));
+
+            if (playerGameScore === parseInt(randomNumber)){
+                winner();
+            } else if (playerGameScore > parseInt(randomNumber)){
+                loser();
+            }
+    });
+};
+const gemPurplePush = function() {
+    $('#purple-gem').click(function() {
+        playerGameScore += parseInt(playerScore) + parseInt(purpleButton);
+        $('#score-counter').text(parseInt(playerGameScore));
+            if (playerGameScore === parseInt(randomNumber)){
+                winner();
+            } else if (playerGameScore > parseInt(randomNumber)){
+                loser();
+            }
+    });
+};
+
+const gemGreenPush = function () {
+    $('#green-gem').click(function() {
+        playerGameScore += parseInt(playerScore) + parseInt(greenButton);
+        $('#score-counter').text(parseInt(playerGameScore));
+            if (playerGameScore === parseInt(randomNumber)){
+                winner();
+            } else if (playerGameScore > parseInt(randomNumber)){
+                loser();
+            }
+    });
+};
+
+
 //initialize game
 const clickToStart = function(){
-    $('#score-counter').empty();
     $('#empty-words').show().animate({right:'50px'});
+    $('#score-counter').empty();
+    playerGameScore=0;
+    updatedLoseCounter="";
+    updatedWinCounter="";
+    crystalButton="";
+    purpleButton="";
+    blueButton="";
+    greenButton="";
     randomNumber = numberArray[Math.floor(Math.random() * numberArray.length)];
-    console.log(randomNumber);
+    //console.log(randomNumber);
     $('.number-generated').text(randomNumber);
     //jewel buttons get new values
     crystalButton=[Math.floor(Math.random()*buttonValue.length)];
     purpleButton=[Math.floor(Math.random()*buttonValue.length)];
     blueButton=[Math.floor(Math.random()*buttonValue.length)];
     greenButton=[Math.floor(Math.random()*buttonValue.length)];
-    console.log(parseInt(crystalButton), parseInt(purpleButton), parseInt(blueButton), parseInt(greenButton));
-    gemPush();
+    //console.log(parseInt(blueButton),parseInt(crystalButton), parseInt(purpleButton),parseInt(greenButton));
+    gemBluePush();
+    gemCrystalPush();
+    gemPurplePush();
+    gemGreenPush();
 };
 
 $('.btn-success').click(clickToStart);
